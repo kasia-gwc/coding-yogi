b7e46e11ac9df363694c38c2ca12588da247bd1d
Kasia Gawelko <kasia.msg@gmail.com>
2020-12-08 22:17:59 +0100

    gatsby created

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,17 @@ module.exports = {
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
-    "gatsby-plugin-manifest",
+    "gatsby-plugin-theme-ui",
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Josefin Sans`,
+            varients: [`200`, `400`],
+          },
+        ],
+      },
+    }
   ],
 };
