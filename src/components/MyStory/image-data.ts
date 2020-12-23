9c7cf748e5cc76d160e4dc49a55575d0f9972627
Kasia Gawelko <kasia.msg@gmail.com>
2020-12-23 20:53:16 +0100

    one removed

--- src/components/MyStory/image-data.ts
+++ src/components/MyStory/image-data.ts
@@ -1,52 +1,42 @@
-export const images = [
-  '/image/petal1.svg',
-  '/image/petals2.svg',
-  '/image/petals3.svg',
-  '/image/petals4.svg',
-  '/image/petals5.svg',
-  '/image/petals6.svg',
-  '/image/petal7.svg',
-  '/image/petals8.svg',
-]
-
 const stories = [
   {
     title: 'Poland',
     description:
       'It’s where I was born, educated and gained my first professional experience as a recruiter.',
-    image: '',
+    image: '/image/petal1.svg',
   },
   {
     title: 'China',
     description:
       'First big solo trip with a noble volunteering plans to save the turtles and clean the ocean.',
+    image: '/image/petals2.svg',
   },
   {
     title: 'Vietnam',
     description:
       'Living on the edge - driving a moped and teaching English Vietnamese children',
-    image: '',
+    image: '/image/petals3.svg',
   },
   {
     title: 'India',
     description: 'I came back to my professional path of being HR generalist',
-    image: '',
+    image: '/image/petals4.svg',
   },
   {
     title: 'England',
     description: 'Continue to work in people function in Fintech sector',
-    image: '',
+    image: '/image/petals6.svg',
   },
   {
     title: 'India',
     description:
       'Getting qualified to teach yoga and landing her freelance job soon after',
-    image: '',
+    image: '/image/petal7.svg',
   },
   {
     title: 'Germany',
     description:
-      'Kasia decides to properly learn coding at Le Wagon and become a coding yogi!',
-    image: '',
+      'Year 2020 offered coding at Le Wagon and become a coding yogi!',
+    image: '/image/petals8.svg',
   },
 ]
