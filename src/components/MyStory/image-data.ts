845a3bc51037238a677e0f3b5dee716263a35916
Kasia Gawelko <kasia.msg@gmail.com>
2020-12-23 20:43:15 +0100

    stories added

--- src/components/MyStory/image-data.ts
+++ src/components/MyStory/image-data.ts
@@ -8,3 +8,45 @@ export const images = [
   '/image/petal7.svg',
   '/image/petals8.svg',
 ]
+
+const stories = [
+  {
+    title: 'Poland',
+    description:
+      'It’s where I was born, educated and gained my first professional experience as a recruiter.',
+    image: '',
+  },
+  {
+    title: 'China',
+    description:
+      'First big solo trip with a noble volunteering plans to save the turtles and clean the ocean.',
+  },
+  {
+    title: 'Vietnam',
+    description:
+      'Living on the edge - driving a moped and teaching English Vietnamese children',
+    image: '',
+  },
+  {
+    title: 'India',
+    description: 'I came back to my professional path of being HR generalist',
+    image: '',
+  },
+  {
+    title: 'England',
+    description: 'Continue to work in people function in Fintech sector',
+    image: '',
+  },
+  {
+    title: 'India',
+    description:
+      'Getting qualified to teach yoga and landing her freelance job soon after',
+    image: '',
+  },
+  {
+    title: 'Germany',
+    description:
+      'Kasia decides to properly learn coding at Le Wagon and become a coding yogi!',
+    image: '',
+  },
+]
